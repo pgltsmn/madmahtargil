36efac7981febf37eebb98ec458885e6d2a7dab5
pgltsmn <93178841+pgltsmn@users.noreply.github.com>
2023-09-19 23:05:26 +0300

    ww

--- app/src/main/java/com/example/madmahtargil/Task3p2.java
+++ app/src/main/java/com/example/madmahtargil/Task3p2.java
@@ -4,10 +4,15 @@ package com.example.madmahtargil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Task3p2 extends AppCompatActivity {
 
     int points = 0;
+    private TextView texthere;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,23 @@ public class Task3p2 extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         assert bundle != null;
         points = bundle.getInt("Points");
+
+        texthere = findViewById(R.id.textView5);
+        image = findViewById(R.id.emoji);
+
+        if (points == 3) {
+            texthere.setText("100!!!!");
+        }
+        else if (points == 2) {
+            texthere.setText("66%.. good enough");
+            image.setImageResource(R.drawable.ok);
+        }
+        else if (points == 1) {
+            texthere.setText("bro.. 33%?");
+            image.setImageResource(R.drawable.sad);
+        }
+        else{
+            texthere.setText("go touch grass with your 0% brain capability");
+        }
     }
 }
